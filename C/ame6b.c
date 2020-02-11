7ce657819d065709a29ef96eaaf2105ac3649cda
ame1 <ame1@csc208.csudh.edu>
2020-02-10 17:03:38 -0800

    finished lab 6 from class

--- C/ame6b.c
+++ C/ame6b.c
@@ -8,4 +8,16 @@ February 10th, 2020
 #include <time.h>
 
 int main(void)
-
+{
+	srand(time(0));
+	for (int i = 0; i < 10; i++)
+	{
+		int x = 0, y = 0, z = 0;
+//		printf("x = %d, y = %d, z = %d\n", x, y, z);
+//		printf("Enter a seed: \n");
+//		scanf("%d", &y);
+		x = rand()%31 + 50;
+//		printf("x = %d, y = %d, z = %d\n", x, y, z);
+		printf("Random number: %d\n", x);
+	}
+}
