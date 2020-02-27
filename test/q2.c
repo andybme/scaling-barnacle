3b64348f5a349db42a4867e886b99fa32c8d7f3a
ame1 <ame1@csc208.csudh.edu>
2020-02-26 16:44:08 -0800

    updated q2

--- test/q2.c
+++ test/q2.c
@@ -13,7 +13,7 @@ int main(void)
 	for (i = 0; i < 11; i++)
 	{
 		x += 2;
-		printf("%.3f \n", x/20);
 	}
+	printf("%.3f \n", x/20);
 	printf("the final value of x is %.3f\n", x);
 }
---
+++ test/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int z = 5, x = 3;
+	float y;
+	y = x/z;
+	printf("%.3f\n", y);
+}
