539681d5396699366ed17fc3d7696f5e6c1e4a8b
ame1 <ame1@csc208.csudh.edu>
2020-02-26 16:55:07 -0800

    question 13

--- test/test.c
+++ test/test.c
@@ -1,9 +1,24 @@
+/*
+Andy Me
+Quiz 2
+Question 12
+*/
 #include <stdio.h>
 
 int main(void)
 {
-	int z = 5, x = 3;
-	float y;
-	y = x/z;
-	printf("%.3f\n", y);
+	char a;
+	int b;
+	float c;
+	double d;
+
+	printf("Please enter a single character:\n");
+	scanf("%c", &a);
+	printf("Please enter an integer:\n");
+	scanf("%d", &b);
+	printf("Please enter a decimal: \n");
+	scanf("%f", &c);
+	printf("Please enter a long decimal: \n");
+	scanf("%f", &d);
+	return 0;
 }
