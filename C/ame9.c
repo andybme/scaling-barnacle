f810baf4e1e286f6553f8c3a9b7f3dd06909b0a3
andybme <andybme@live.com>
2020-04-09 19:08:01 -0700

    more working on lab 9

--- C/ame9.c
+++ C/ame9.c
@@ -8,10 +8,10 @@ Lab 9
 #include <ctype.h>
 #include <string.h>
 
-void printVowels(char charr);
-void printLower(char charr);
-void printUpper(char charr);
-int getNumber(char charr);
+void printVowels(int *charr);
+void printLower(int *charr);
+void printUpper(int *charr);
+int getNumber(int *charr);
 
 int main(void)
 {
@@ -19,19 +19,29 @@ int main(void)
     char charr[20];
     printf("Enter a string less than 20 characters that includes one(1) number:\n");
     fgets(charr, 20, stdin);
-    printLower(charr);
-    printUpper(charr);
-    num = getNumber(charr);
+    printVowels(charr);
+    // printLower(charr);
+    // printUpper(charr);
+    // num = getNumber(charr);
 
-    printf("Number entered in string: %d, * 12 = %d", num, num*12);
+    // printf("Number entered in string: %d, * 12 = %d", num, num*12);
 
 }
 
-void printVowels(char charr)
+void printVowels(int *charr)
 {
-    int *ptr;
+    int *ptr, *i;
+    int j, counter;
+    int vowelAscii[12] = {65, 69, 73, 79, 85, 89, 97, 101, 105, 111, 117, 121};
     for (ptr = charr; ptr < charr + 20; ptr++)
     {
-        if(isVowel)
+        for(i = vowelAscii; i < vowelAscii + 12; i++)
+        {
+            if(*ptr = *i)
+            {
+                counter++;
+            }
+        }
     }
+    printf("Number of Vowels:\n%d\n", counter);
 }
